9e32e4f0dc94c7d8bb019188a26fc3c112368b7e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-11-26 14:03:34 +0100

    first unit tests

--- test/FileSystemSpec.hs
+++ test/FileSystemSpec.hs
@@ -10,6 +10,14 @@ import Test.Validity
 
 import FileSystem
 
+-- stack test --file-watch
+-- writeFile :: String -> String -> FileSystem -> FileSystem
+-- readFile :: String -> FileSystem -> Maybe String
 spec :: Spec
 spec = do
-  pure ()
+  describe "writeFile" $ do
+    it "writes this example file to an empty file system" $
+      writeFile "path" "contents" (FileSystem []) `shouldBe` (FileSystem [("path", "contents")])
+    it "writes this example file to a non-empty file system" $
+      writeFile "path2" "contents2" (FileSystem [("path1", "contents1")]) `shouldBe`
+      (FileSystem [("path1", "contents1"), ("path2", "contents2")])
