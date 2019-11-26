e33d9f974b5b9788d0fe658b35f0e9d487acc4f1
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-11-26 14:15:15 +0100

    second bit

--- test/FileSystemSpec.hs
+++ test/FileSystemSpec.hs
@@ -13,6 +13,9 @@ import FileSystem
 -- stack test --file-watch
 -- writeFile :: String -> String -> FileSystem -> FileSystem
 -- readFile :: String -> FileSystem -> Maybe String
+instance Arbitrary FileSystem where
+  arbitrary = FileSystem <$> arbitrary
+
 spec :: Spec
 spec = do
   describe "writeFile" $ do
@@ -21,3 +24,19 @@ spec = do
     it "writes this example file to a non-empty file system" $
       writeFile "path2" "contents2" (FileSystem [("path1", "contents1")]) `shouldBe`
       (FileSystem [("path1", "contents1"), ("path2", "contents2")])
+    it "is idempotent" $ do
+      property $ \fs ->
+        property $ \path ->
+          property $ \contents ->
+            let fs1 = writeFile path contents fs
+             in writeFile path contents fs1 `shouldBe` fs1
+  describe "readFile" $ do
+    it "reads the file that was just written" $
+      property $ \fs ->
+        property $ \path ->
+          property $ \contents ->
+            readFile path (writeFile path contents fs) `shouldBe` Just contents
+    it "writing a file that already existed with the same contents, doesn't change anything" $
+      forAll (arbitrary `suchThat` (not . null . fileSystemFiles)) $ \fs ->
+        let (FileSystem files) = fs
+         in forAll (elements files) $ \(path, contents) -> writeFile path contents fs `shouldBe` fs
